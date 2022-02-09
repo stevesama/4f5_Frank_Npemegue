564dedf2dfe1ed5751987fb283d1358b6cb5b534
frank <stevekeunou@gmail.com>
2022-02-08 21:31:15 -0500

    module 2

---
+++ tutoriels/pong/src/main/java/pong/ClientPong.java
@@ -0,0 +1,41 @@
+package pong;
+
+import ca.ntro.app.NtroClientFx;
+import ca.ntro.app.backend.BackendRegistrar;
+import ca.ntro.app.frontend.FrontendRegistrarFx;
+import ca.ntro.app.messages.MessageRegistrar;
+import ca.ntro.app.models.ModelRegistrar;
+import pong.frontal.FrontalPong;
+
+public class ClientPong implements NtroClientFx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NtroClientFx.launch(args);
+	}
+
+	@Override
+	public void registerBackend(BackendRegistrar registrar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerFrontend(FrontendRegistrarFx registrar) {
+		// TODO Auto-generated method stub
+		registrar.registerFrontend(new FrontalPong());
+	}
+
+	@Override
+	public void registerMessages(MessageRegistrar registrar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerModels(ModelRegistrar registrar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

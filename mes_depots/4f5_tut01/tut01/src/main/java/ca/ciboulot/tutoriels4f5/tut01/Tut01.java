1fa86c9e1311ac7c5b0f59dddaa32a8f383eab24
frank <stevekeunou@gmail.com>
2022-02-08 21:28:53 -0500

    tutoriels/module 2

---
+++ mes_depots/4f5_tut01/tut01/src/main/java/ca/ciboulot/tutoriels4f5/tut01/Tut01.java
@@ -0,0 +1,12 @@
+package ca.ciboulot.tutoriels4f5.tut01;
+
+import MON_JEU.MON_NOM.MonJeuMonNom;
+import ca.ntro.app.NtroAppFx;
+
+public class Tut01 {
+
+	public static void main(String[] args) {
+		NtroAppFx.launch(MonJeuMonNom.class, args);
+	}
+
+}

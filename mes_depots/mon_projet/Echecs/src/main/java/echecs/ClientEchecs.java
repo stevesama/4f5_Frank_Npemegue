7fb6056e83cd978fec008470da8e57ee524fde5b
frank <stevekeunou@gmail.com>
2022-02-10 22:12:24 -0500

    Objectif 2

---
+++ mes_depots/mon_projet/Echecs/src/main/java/echecs/ClientEchecs.java
@@ -0,0 +1,42 @@
+package echecs;
+
+import ca.ntro.app.NtroClientFx;
+import ca.ntro.app.backend.BackendRegistrar;
+import ca.ntro.app.frontend.FrontendRegistrarFx;
+import ca.ntro.app.messages.MessageRegistrar;
+import ca.ntro.app.models.ModelRegistrar;
+import echecs.frontal.FrontalEchecs;
+
+public class ClientEchecs implements NtroClientFx{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		NtroClientFx.launch(args);
+	}
+
+	@Override
+	public void registerBackend(BackendRegistrar registrar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerFrontend(FrontendRegistrarFx registrar) {
+		// TODO Auto-generated method stub
+		registrar.registerFrontend(new FrontalEchecs());
+	}
+
+	@Override
+	public void registerMessages(MessageRegistrar registrar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerModels(ModelRegistrar registrar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
